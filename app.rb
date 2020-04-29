9e743f42b96882df9c39e229cee11764925a9339
marcon29 <online@markstabler.com>
2020-04-29 11:28:39 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  # home page
+  get '/' do
+    "Hello, World!"
+  end
+
+  # change url to add pages (new route)
+  get '/test' do
+    "Hello World! from the test page"
+  end
 
 
 end
