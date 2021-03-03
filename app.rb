212cc65bb34ce584601aa4b7ae2553f27cc36f4a
danny galimi <dannygalimi13@aim.com>
2021-03-03 04:04:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require_relative 'config/environment'
+<<<<<<< HEAD
 
 class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
 end
+=======
+
+class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+end
+
+>>>>>>> 2f8e162d03ebe7c46e063fbe429d30340e5b96bf
 
 
